e4fc1c313bacd85d0a4e0f68a07416bbb87425c3
vinimello90 <vinicius.barretto9022@gmail.com>
2025-03-25 20:30:00 -0300

    refactor: separate cards and users routes into modules

---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const CARDS_PATH = path.join(__dirname, '../data/cards.json');
+
+router.get('/cards', async (req, res) => {
+  try {
+    const data = await fsPromises.readFile(CARDS_PATH, { encoding: 'utf8' });
+    res.send(JSON.parse(data));
+  } catch (err) {
+    res.status(500).send({ message: 'Ocorreu um erro ao buscar os dados' });
+  }
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const USERS_PATH = path.join(__dirname, '../data/users.json');
+
+router.get('/users', async (req, res) => {
+  try {
+    const data = await fsPromises.readFile(USERS_PATH, { encoding: 'utf8' });
+    res.send(JSON.parse(data));
+  } catch (err) {
+    res.status(500).send({ message: 'Ocorreu um erro ao buscar os dados' });
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(
+      await fsPromises.readFile(USERS_PATH, { encoding: 'utf8' }),
+    );
+    const userData = data.find((user) => user._id === req.params.id);
+    if (!userData) {
+      res.status(404).send({ message: 'ID do usuário não encontrado' });
+      return;
+    }
+    res.send(userData);
+  } catch (err) {
+    res.status(500).send({ message: 'Ocorreu um erro ao buscar os dados' });
+    console.error(err);
+  }
+});
+
+module.exports = router;
