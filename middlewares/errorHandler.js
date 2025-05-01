cba51cd935a5c956900f02f7071410507383c1f1
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-01 12:58:52 -0300

    refactor: simplify errorHandler logic

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,21 +1,20 @@
 const BAD_REQUEST = 400;
-const UNAUTHORIZED = 401;
-const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = (err, req, res, next) => {
-  if (err.name === 'ValidationError') {
+  const { name, statusCode = 500, message } = err;
+  if (name === 'ValidationError') {
     const errorMessage = Object.values(err.errors).map((e) => e.message);
     res.status(BAD_REQUEST).send({ message: `${errorMessage}` });
     return;
   }
-  if (err.name === 'CastError') {
+  if (name === 'CastError') {
     res.status(BAD_REQUEST).send({ message: 'The provided ID is invalid.' });
     return;
   }
-  if (err.statusCode === NOT_FOUND) {
-    res.status(NOT_FOUND).send({ message: err.message });
-    return;
-  }
-  res.status(INTERNAL_SERVER_ERROR).send({ error: 'Internal Server Error.' });
+
+  res.status(statusCode).send({
+    error:
+      statusCode !== INTERNAL_SERVER_ERROR ? message : 'Internal Server Error.',
+  });
 };
