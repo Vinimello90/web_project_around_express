f0b2c50ad0e4efb75752ba7a93937da69cb67882
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-01 12:17:48 -0300

    feat: add NotFoundError class and error code constants to errorHandler middleware

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,16 +1,21 @@
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
+const INTERNAL_SERVER_ERROR = 500;
+
 module.exports = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
     const errorMessage = Object.values(err.errors).map((e) => e.message);
-    res.status(400).send({ message: `${errorMessage}` });
+    res.status(BAD_REQUEST).send({ message: `${errorMessage}` });
     return;
   }
   if (err.name === 'CastError') {
-    res.status(400).send({ message: 'The provided ID is invalid.' });
+    res.status(BAD_REQUEST).send({ message: 'The provided ID is invalid.' });
     return;
   }
-  if (err.statusCode === 404) {
-    res.status(err.statusCode).send({ message: err.message });
+  if (err.statusCode === NOT_FOUND) {
+    res.status(NOT_FOUND).send({ message: err.message });
     return;
   }
-  res.status(500).send({ error: 'Internal Server Error.' });
+  res.status(INTERNAL_SERVER_ERROR).send({ error: 'Internal Server Error.' });
 };
